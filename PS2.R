bef0071fe1cdfcae76cf155bffe8560e3c8184aa
Benjamin Schneider <benjamin.schneider@wustl.edu>
2018-02-06 10:28:28 -0600

    1. Documentation and cleaning
    cleaned up some of the var names for clarity, documentation of what the code does in notes

--- PS2.R
+++ PS2.R
@@ -5,60 +5,62 @@
 
 a<-c(345,6758,4563,5968,128,457,589,783,90,274,346) #Example of an input vector of vote returns. Just me typing random keys.
 leemis<-function(a){
-  b<-as.numeric(substr(a, start=1, stop=1))
+  firstnumber<-as.numeric(substr(a, start=1, stop=1)) #this takes just the first number of the vector of vote totals and makes new vector.
   proportions<-NULL
-  for (i in 1:9){
-    y<-sum(b==i)/length(a)
-    proportions<-c(proportions,y)
-  }
-  xi<-proportions
+  for (i in 1:9){ #creates x_i for the for loop
+    y<-sum(firstnumber==i)/length(a)
+    proportions<-c(proportions,y)}
+  x_i<-proportions #x_i made
   argument<-NULL
   for(i in 1:9){
-    func<-(xi[i]-log((1+1/i), base=10))
-    fullfunc<-c(argument,func)
-  }
-  maxfunc<-max(fullfunc)
-  return(maxfunc)
-}
-leemis(a) #this is an example of running this equation
-chovec<-NULL
-chogains<-function(a){
-  b<-as.numeric(substr(a, start=1, stop=1)) 
-  proportions<-NULL
-  for (i in 1:9){
-    y<-sum(b==i)/length(a)
-    proportions<-c(proportions,y)
-  }
-  xi<-proportions
-  argument<-NULL
-  for(i in 1:9){
-    element<-((xi[i]-log((1+1/i), base=10))^2)
-    chovec<-c(chovec,element)
-    finalvec<-sum(chovec)
-    output<-sqrt(finalvec)
-  }
+    func<-(x_i[i]-log((1+1/i), base=10)) #executing the equations
+    fullfunc<-c(argument,func)}
+  output<-max(fullfunc) #taking the max of the equation
   return(output)
 }
-chogains(a) #output is number of the output vector
 
-benford.function<-function(a,leemis,chogains){
-  if(leemis==T){
-    leemis<-leemis(a)} 
-  else{
-    leemis<-NULL}
-  if(chogains==T){
-    chogains<-chogains(a)} 
-  else{
-    chogains<-NULL}
-  digitdist<-table(as.numeric(substr(a, start=1, stop=1)))
-  answer<-list(leemis,chogains,digitdist) 
-  names(answer)<-c("Leemis", 'Cho-Gains', "Distribution of Digits")
-  return(answer)
+leemis(a) #this is an example of running this function
+
+chovec<-NULL
+chogains<-function(a){
+  firstnumber<-as.numeric(substr(a, start=1, stop=1)) #this takes just the first number of the vector of vote totals and makes new vector.
+  proportions<-NULL
+  for (i in 1:9){ #creates x_i for the for loop
+    y<-sum(firstnumber==i)/length(a)
+    proportions<-c(proportions,y)}
+  x_i<-proportions #x_i made
+  argument<-NULL
+  for(i in 1:9){
+    element<-((x_i[i]-log((1+1/i), base=10))^2) #calculation inside summation
+    chovec<-c(chovec,element)
+    finalvec<-sum(chovec) #the summation
+    output<-sqrt(finalvec)} #squre root of the summation
+  return(output)
 }
-benford.function(a,T,F)
-benford.function(a,F,T)
-benford.function(a,T,T)
-benford.function(a,F,F)
+
+chogains(a) #example of function
+
+#Great, so now that those are done, I am going to use them into this new function that will do all of the above
+#separating them is useful because when I tried doing them all together making mistakes was much easier.
+benford.function<-function(a,Leemis, ChoGains){
+  if(Leemis==T){ #if else used to print the leemis
+    Leemis<-leemis(a)} #use of the leemis function written above
+  else{
+    Leemis<-"You did not request this output"} #inclusion of this for clarity
+  if(ChoGains==T){ #if else for Cho
+    ChoGains<-chogains(a)} #use of chogains function written above
+  else{
+    ChoGains<-"You did not request this output"} #once again included for clarity
+  digitdist<-table(as.numeric(substr(a, start=1, stop=1))) #this shows the frequency of the various first digits
+  output<-list(Leemis,ChoGains,digitdist) #output as a list
+  names(output)<-c("Leemis", 'Cho-Gains', "Distribution of Digits")
+  return(output)
+}
+#some examples of how the function works with its different arguments and outputs
+benford.function(a,T,F) #Only the leemis is being printed
+benford.function(a,F,T) #Only the Cho-Gains is being printed
+benford.function(a,T,T) #both printed
+benford.function(a,F,F) #neither printed
 
 
 #Problem 2
